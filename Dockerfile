f063a3de9407836d0e9a07cfaea875fbace1269f
sergkstudio <sergkstudio@gmail.com>
2024-10-09 11:45:32 +0300

    update

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,6 @@ FROM python:3.9-slim
 
 # Устанавливаем зависимости
 WORKDIR /app
-COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
 
 # Копируем исходный код
--- app.py
+++ app.py
@@ -7,43 +7,9 @@ app = Flask(__name__)
 r = redis.Redis(host='redis', port=6379)
 
 # Главная страница заработной платы
-@app.route('/salary', methods=['GET', 'POST'])
-def salary():
-    if request.method == 'POST':
-        company = request.form['company']
-        salary = request.form['salary']
-        r.hset('salaries', company, salary)
-    salaries = r.hgetall('salaries')
-    return render_template('salary.html', salaries=salaries)
-
-# Страница обязательных расходов
-@app.route('/expenses', methods=['GET', 'POST'])
-def expenses():
-    if request.method == 'POST':
-        expense_name = request.form['expense_name']
-        expense_amount = request.form['expense_amount']
-        r.hset('expenses', expense_name, expense_amount)
-    expenses = r.hgetall('expenses')
-    return render_template('expenses.html', expenses=expenses)
-
-# Страница для ввода данных
-@app.route('/enter_data', methods=['GET', 'POST'])
-def enter_data():
-    if request.method == 'POST':
-        amount = request.form['amount']
-        type_ = request.form['type']
-        comment = request.form['comment']
-        if type_ == 'income':
-            r.lpush('accounting', f'Debet: {amount} | Комментарий: {comment}')
-        elif type_ == 'expense':
-            r.lpush('accounting', f'Кредит: {amount} | Комментарий: {comment}')
-    return render_template('enter_data.html')
-
-# Страница таблицы учёта
-@app.route('/accounting')
-def accounting():
-    records = r.lrange('accounting', 0, -1)
-    return render_template('accounting.html', records=records)
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
