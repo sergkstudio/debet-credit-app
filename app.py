395da5e2a3656c96dc27d10f21ff63def174ac1d
sergkstudio <sergkstudio@gmail.com>
2024-10-09 11:57:28 +0300

    2

--- app.py
+++ app.py
@@ -1,15 +1,38 @@
-from flask import Flask, render_template, request, redirect, url_for
-import redis
+import logging
+from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
 
-# Настройка Redis
-r = redis.Redis(host='redis', port=6379)
+# Настройка логирования
+logging.basicConfig(level=logging.INFO)
 
-# Главная страница заработной платы
+# Подключение БД
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def close_db_connection(conn):
+    conn.close()
+
+# Инициализация БД
+def init_db():
+    conn = get_db_connection()
+    conn.execute('CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, content TEXT NOT NULL)')
+    conn.close()
+
+@app.before_first_request
+def before_first_request():
+    init_db()
+
+# Главная страница
 @app.route('/')
 def index():
-    return render_template('index.html')
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
