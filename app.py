6c952ad34ace1878c85efffe7eb1045bf04c3534
sergkstudio <sergkstudio@gmail.com>
2024-10-09 12:47:42 +0300

    1

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
-import logging
 from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 
 app = Flask(__name__)
-
-# Настройка логирования
-logging.basicConfig(level=logging.INFO)
 
 # Подключение БД
 def get_db_connection():
@@ -13,12 +9,10 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
-def close_db_connection(conn):
-    conn.close()
-
 # Инициализация БД
 def init_db():
     conn = get_db_connection()
+    
     # Создаем таблицу для заработной платы
     conn.execute('''
         CREATE TABLE IF NOT EXISTS salary (
@@ -45,35 +39,39 @@ def init_db():
             comment TEXT
         )
     ''')
+    
     conn.close()
 
-# Инициализация БД при старте приложения
 with app.app_context():
     init_db()
 
-# Страница для добавления новых данных
+# Главная страница с добавлением данных и отображением таблиц
 @app.route('/', methods=['GET', 'POST'])
-def new_entry():
+def index():
+    conn = get_db_connection()
+    
+    # Если запрос POST, обрабатываем добавление записи
     if request.method == 'POST':
-        # Получаем данные из формы
         table = request.form['table']
         size = request.form['size']
         comment = request.form['comment']
-
-        # Вставляем данные в нужную таблицу
-        conn = get_db_connection()
+        
         if table == 'salary':
             conn.execute('INSERT INTO salary (size, comment) VALUES (?, ?)', (size, comment))
         elif table == 'obligatory_expenses':
-            conn.execute('INSERT INTO obligatory_expenses (size, comment) VALUES (?, ?)')
+            conn.execute('INSERT INTO obligatory_expenses (size, comment) VALUES (?, ?)', (size, comment))
         elif table == 'income_expenses':
-            conn.execute('INSERT INTO income_expenses (size, comment) VALUES (?, ?)')
+            conn.execute('INSERT INTO income_expenses (size, comment) VALUES (?, ?)', (size, comment))
         
         conn.commit()
-        conn.close()
-        return redirect(url_for('index'))
+    
+    # Получаем записи из всех таблиц
+    salary = conn.execute('SELECT * FROM salary').fetchall()
+    obligatory_expenses = conn.execute('SELECT * FROM obligatory_expenses').fetchall()
+    income_expenses = conn.execute('SELECT * FROM income_expenses').fetchall()
+    conn.close()
 
-    return render_template('index.html')
+    return render_template('index.html', salary=salary, obligatory_expenses=obligatory_expenses, income_expenses=income_expenses)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
